7c6e9a54dadd8dd194c42f360bb4d343d0979c34
James Rasche <rasjm001@mymail.unisa.edu.au>
2024-12-28 23:22:12 +1030

    update top nav bar links

--- canvas.js
+++ canvas.js
@@ -1,3 +1,13 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function topbar_navigation_function() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
 //console.log('utilising this canvas')
 var canvas = document.querySelector('canvas');
 
@@ -137,7 +147,9 @@ window.addEventListener("resize", function(){
     init();
 })
 
-
+window.addEventListener("click", function(event){
+    console.log("has been clicked");
+})
 
 
 
